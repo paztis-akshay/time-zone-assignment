0d5fb2c3a875f956593b52f118ca5c13c226446b
Akshay Agrawal <akshay.agrawal2595@gmail.com>
2021-04-17 23:43:10 +0530

    updated useEffect

--- src/components/timeZone/TimeZone.js
+++ src/components/timeZone/TimeZone.js
@@ -1,15 +1,15 @@
 import React , {useEffect, useState} from 'react';
 import axios from 'axios';
-import {GETTIMEZONEURL, INTERVAL} from '../../config/time-zone-config';
+import {GETTIMEZONEURL, INTERVAL, GETZONESLISTURL} from '../../config/time-zone-config';
 
 export const TimeZoneComponent = (props) => {
 
-    const [selectedTimeZone, setSelectedTimeZone] = useState({});
+    const [selectedTimeZone, setSelectedTimeZone] = useState('');
     const [timeZones, setTimeZones] = useState([]);
     const [currTime, setCurrTime] = useState('');
 
     useEffect(() => {
-        axios.get(GETTIMEZONEURL, {crossdomain: true}).then(res => {
+        axios.get(GETZONESLISTURL, {crossdomain: true}).then(res => {
           setTimeZones(res.data.zones)
         })
         .catch((err => {
@@ -17,37 +17,35 @@ export const TimeZoneComponent = (props) => {
         }));
       }, []);
 
-    function updateSelectedTimeZone() {
-        // eslint-disable-next-line no-useless-concat
-        axios.get(GETTIMEZONEURL + '/' + `${selectedTimeZone.countryName}`, {crossdomain: true}).then(res =>  {
-            setCurrTime(res.data.currTime.formatted);
-            updateTimeZone();
-        })
+    function getTimeZone(selectedZone) {
+        axios.get(GETTIMEZONEURL + `${selectedZone}`, {crossdomain: true}).then(res =>  {
+            setCurrTime(res.data?.currTime?.formatted);
+        });
     }
 
-    function updateTimeZone() {
-        setInterval(() => {
-            updateSelectedTimeZone();
-        }, INTERVAL)
-    }
+    useEffect(() => {
+        if(selectedTimeZone.length) {
+            getTimeZone(selectedTimeZone);
+            setInterval(() => {
+                getTimeZone(selectedTimeZone);
+            }, INTERVAL);
+        }
+    }, [selectedTimeZone]);
 
     function handleDropdownChange(e) {
-        setSelectedTimeZone(e);
-        updateSelectedTimeZone();
+        setSelectedTimeZone(e.target.value);
     }
 
     return (
             <div className = 'box'>
-                <div className = 'dropdown-box'>
-                    <select id="dropdown" onChange={e => handleDropdownChange(e.target.value)}>
+                    <select id="dropdown" value={selectedTimeZone} onChange={handleDropdownChange}>
                         {
                             timeZones.map((item, index) => {
-                                return <option key={index} value={item.counryName}>{item.zoneName}</option>
+                                return <option key={index} value={item.countryName}>{item.zoneName}</option>
                             })
                         }
                     </select>
-                </div>
-                {currTime.length ? <div>{props.currTime}</div> : ''}
+                {currTime?.length ? <div>{props.currTime}</div> : ''}
             </div>
     )
 }
--- src/config/time-zone-config.js
+++ src/config/time-zone-config.js
@@ -1,2 +1,3 @@
-export const GETTIMEZONEURL = 'http://api.timezonedb.com/v2.1/list-time-zone?key=XWSLLPX5RMIZ&format=json&zone=Europe';
+export const GETZONESLISTURL = 'http://api.timezonedb.com/v2.1/list-time-zone?key=XWSLLPX5RMIZ&format=json&zone=Europe';
+export const GETTIMEZONEURL = 'Http://api.timezonedb.com/v2/get-time-zone?key=XWSLLPX5RMIZ&format=json&by=zone&zone=Europe/';
 export const INTERVAL = 5000;
